05e13d288d270c49af1b9fa633ac3901fa9ecf2d
Joshua Ojeda <framehousestudios2001@gmail.com>
2026-02-16 17:08:21 -0800

    Initial Commit

---
+++ them/themApp.swift
@@ -0,0 +1,17 @@
+//
+//  themApp.swift
+//  them
+//
+//  Created by Joshua Ojeda on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct themApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
